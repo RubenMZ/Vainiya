f4f8b0e731f71b763a80594c1723a60479d2fae8
Ruben <i32mezar@uco.es>
2015-02-15 14:04:57 +0100

    Añadido el xml del listView

---
+++ app/src/main/java/com/example/rubn/vainiya/Contacto.java
@@ -0,0 +1,42 @@
+package com.example.rubn.vainiya;
+
+/**
+ * Created by Rubén on 13/02/2015.
+ */
+public class Contacto {
+
+    String nombre, tlf, email, contraseña;
+
+
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
--- app/src/main/java/com/example/rubn/vainiya/MainActivity.java
+++ app/src/main/java/com/example/rubn/vainiya/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.rubn.vainiya;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
     EditText nombre, tlf, email, pass;
     Button btAgregar;
+    Switch btSexo;
+    TextView tvSexo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +29,43 @@ public class MainActivity extends ActionBarActivity {
         tlf = (EditText) findViewById(R.id.editText2);
         email = (EditText) findViewById(R.id.editText3);
         pass = (EditText) findViewById(R.id.editText4);
+
+        tvSexo = (TextView) findViewById(R.id.tvSexo);
         btAgregar = (Button) findViewById(R.id.btAgregar);
+        btSexo = (Switch) findViewById(R.id.btSexo);
 
         btAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast mensaje =  Toast.makeText(getApplicationContext(), "Contacto agregado", Toast.LENGTH_LONG);
+                mensaje.show();
+                limpiarCampos();
             }
         });
 
+    }
+
+    private void limpiarCampos(){
+        nombre.getText().clear();
+        tlf.getText().clear();
+        email.getText().clear();
+        pass.getText().clear();
+        limpiarCampos();
+    }
+
+    public void onToggleClicked(View view) {
+        // Is the toggle on?
+        boolean on = ((Switch) view).isChecked();
+
+        if (on) {
+            Vibrator v = (Vibrator) getSystemService(getApplicationContext().VIBRATOR_SERVICE);
+            v.vibrate(250);
+            tvSexo.setText("Mujer");
+        } else {
+            Vibrator v = (Vibrator) getSystemService(getApplicationContext().VIBRATOR_SERVICE);
+            v.vibrate(500);
+            tvSexo.setText("Hombre");
+        }
     }
 
 
